4e94eee49fa25952ff7acc92197e539c14adeec2
prashansapkota <prashan.sapkota3456@gmail.com>
2024-12-01 11:58:26 -0600

    Added images to about-fisk

--- image_classifcation.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from keras.preprocessing.image import image
-from keras.preprocessing.image import img_to_array
-from keras.applications.resnet50 import preprocess_input
-from keras.applications.imagenet_utils import decode_predictions
-import matplotlib.pyplot as plt
-
-
